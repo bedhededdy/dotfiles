f99b75894846770d214db4fe1018cca1e0cbdbc0
Edward Pinkston <edward.pinkston@gmail.com>
2024-09-09 01:41:27 -0500

    Change tsserver to ts_ls

--- init.lua
+++ init.lua
@@ -211,7 +211,7 @@ local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("mason").setup({})
 require("mason-lspconfig").setup({
     ensure_installed = {
-        "tsserver",
+        "ts_ls",
         "lua_ls",
         "html",
         "cssls",
